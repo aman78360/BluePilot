d417b0e7df0542717ea374b3599f4b4819c18338
Aman Gupta <guptaaman1235@gmail.com>
2022-05-17 19:53:53 +0530

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// // Script for Scroll Animation 
+// const cards = document.querySelectorAll(".c-hide")
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         entry.target.classList.toggle("c-show", entry.isIntersecting)
+//     })
+// },{
+//     threshold: 0.5
+// })
+// cards.forEach(cards => {
+//     observer.observe(cards)
+// })
+
+
+
+
+
+
